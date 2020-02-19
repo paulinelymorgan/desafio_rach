2ba4d21c9e5f07b383cf93dbe5e881ecbf4f9213
Paulinely Morgan <paulinelym@gmail.com>
2020-02-19 18:07:53 -0300

    todas as metricas, questao 3

--- rach/primeira_etapa.py
+++ rach/primeira_etapa.py
@@ -101,10 +101,10 @@ if gerarGraficos:
 print("questao 2: ")
 
 # extraindo apenas os tres parametros
-questao2 = pd.DataFrame({'Timestamp': pd.to_datetime(base.Date), 'Texto': base.Full_Text, 'IsSpam': base.IsSpam})
+questao2 = pd.DataFrame({'Date': pd.to_datetime(base.Date), 'Text': base.Full_Text, 'IsSpam': base.IsSpam})
 
 # criando um novo parametro (mes/ano)
-questao2['Month/Year'] = questao2['Timestamp'].apply(lambda x: "%d/%d" % (x.month, x.year))
+questao2['Month/Year'] = questao2['Date'].apply(lambda x: "%d/%d" % (x.month, x.year))
 # print(questao2)
 
 # agrupa pelos dois parametros (o novo gerado e 'IsSpam')
@@ -117,19 +117,65 @@ print(classificacoes_por_mes_q2)
 #### QUESTAO 2 - palavras mais frequentes por mes
 
 
-#### QUESTAO 3 - palavras mais frequentes por mes
-print("questao 3: ")
+#### QUESTAO 3 - varias metricas
+print("\nquestao 3: ")
 
 # extraindo apenas os dois parametros
-questao3 = pd.DataFrame({'Timestamp': pd.to_datetime(base.Date), 'Word_Count': base.Word_Count})
+questao3 = pd.DataFrame({'Date': pd.to_datetime(base.Date), 'Word_Count': base.Word_Count})
+# questao3_2 = pd.DataFrame({'Date': pd.to_datetime(base.Date), 'Word_Count': base.Word_Count, '': base.Word})
+# questao3_2 = questao3
 
 # criando um novo parametro (mes/ano)
-questao3['Month/Year'] = questao3['Timestamp'].apply(lambda x: "%d/%d" % (x.month, x.year))
-# print(questao3)
+questao3['Month/Year'] = questao3['Date'].apply(lambda x: "%d/%d" % (x.month, x.year))
+# questao3_2['Month/Year'] = questao3_2['Date'].apply(lambda x: "%d/%d" % (x.month, x.year))
 
 # agrupa pelos dois parametros (o novo gerado e 'Word_Count')
-agrupado_mes_q3 = questao3.groupby(['Month/Year', 'Word_Count'])
+print(questao3)
+agrupado_mes_q3 = questao3.groupby(['Month/Year'])
+
+print()
+
+print("\nsize: {}".format(agrupado_mes_q3.size()))
+print("\nmean: {}".format(agrupado_mes_q3.mean().rename(columns={'Word_Count':'MEAN'})))
+print("\nstd: {}".format(agrupado_mes_q3.std().rename(columns={'Word_Count':'STD'})))
+print("\nmax: {}".format(agrupado_mes_q3.max().rename(columns={'Word_Count':'MAX'})))
+print("\nmin: {}".format(agrupado_mes_q3.min().rename(columns={'Word_Count':'MIN'})))
+
+#### QUESTAO 3 - varias metricas
+
+
+# agrupado_mes_q3_2 = questao3.groupby(['Month/Year', 'Word_Count'])
+# print(agrupado_mes_q3_2.size())
+# print(agrupado_mes_q3_2.size().mean())
+
+# print(questao3_2)
+# testando = agrupado_mes_q3.groupby(['Month/Year', 'Word_Count']).agg({'text':'size', 'Word_Count':'mean'})\
+#     .rename(columns={'text':'count','sent':'mean_sent'})\
+#     .reset_index()
+
+# testando = questao3.groupby(['Month/Year', 'Word_Count'])\
+#     .agg({'Date':'size', 'Common_Word_Count':'mean'})\
+#     .rename(columns={'Date':'Size', 'Common_Word_Count':'mean_'})\
+#     .reset_index()
+
+# testando = questao3.groupby(['Month/Year'])\
+#     .agg({'Date':'size', 'Common_Word_Count':'mean'})\
+#     .rename(columns={'Date':'Size', 'Common_Word_Count':'mean_'})\
+#     .reset_index()
 
 # agrega com o tamanho
-classificacoes_por_mes_q3 = agrupado_mes_q3.size()
-print(classificacoes_por_mes_q3)
+# classificacoes_por_mes_q3 = agrupado_mes_q3.size()
+# testando = testando
+
+
+# print(testando)
+# print(classificacoes_por_mes_q3)
+# print(classificacoes_por_mes_q3.get_values())
+
+# classificacoes_por_mes_q3.to_pickle("file_name.txt")
+# import h5py
+# from pandas import HDFStore,DataFrame
+# store = HDFStore('store.h5')
+# store = pd.HDFStore('q3.h5')
+# store['classificacoes_por_mes_q3'] = classificacoes_por_mes_q3  # save it
+# store['classificacoes_por_mes_q3']  # load it
