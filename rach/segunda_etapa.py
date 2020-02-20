eddc057655dda866660489176d6b5cbef4ef818d
Ana Raquel <anaraqueldemorais@gmail.com>
2020-02-19 22:22:42 -0300

    código fazendo o tratamento das mensagens

--- rach/segunda_etapa.py
+++ rach/segunda_etapa.py
@@ -6,7 +6,14 @@ import matplotlib.pyplot as plt
 import datetime
 import sklearn
 from sklearn import preprocessing
+from sklearn import svm
+from sklearn.model_selection import (
+    train_test_split, learning_curve, StratifiedShuffleSplit, GridSearchCV,
+    cross_val_score)
 import re
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 # %matplotlib inline
 
 # Warnings
@@ -28,10 +35,6 @@ endereco_arquivo = 'C:/Users/Ana Raquel/Senior/desafio/sms_senior.csv'
 base = pd.read_csv(endereco_arquivo, encoding = "ISO-8859-1")
 ### carregando dados
 
-example = """  ***** CONGRATlations **** You won 2 tIckETs to Hamilton in 
-NYC http://www.hamiltonbroadway.com/J?NaIOl/event   wORtH over $500.00...CALL 
-555-477-8914 or send message to: hamilton@freetix.com to get ticket !! !  """
-
 ### infomacoes da base
 #print()
 #base.info()
@@ -39,6 +42,12 @@ NYC http://www.hamiltonbroadway.com/J?NaIOl/event   wORtH over $500.00...CALL
 
 y = base['IsSpam'].value_counts()
 print(y)
+
+#Normalização
+
+example = """  ***** CONGRATlations **** You won 2 tIckETs to Hamilton in 
+NYC http://www.hamiltonbroadway.com/J?NaIOl/event   wORtH over $500.00...CALL 
+555-477-8914 or send message to: hamilton@freetix.com to get ticket !! !  """
 
 le = preprocessing.LabelEncoder()
 y_enc = le.fit_transform(y)
@@ -59,11 +68,15 @@ processed = processed.str.replace(r'^\s+|\s+?$', '')
 
 processed = processed.str.lower()
 
+#Stop Words
+
 stop_words = nltk.corpus.stopwords.words('english')
 
 processed = processed.apply(lambda x: ' '.join(
     term for term in x.split() if term not in set(stop_words))
 )
+
+#Stemming
 
 porter = nltk.PorterStemmer()
 processed = processed.apply(lambda x: ' '.join(
@@ -93,3 +106,76 @@ print(preprocess_text(example))
 
 print((processed == raw_text.apply(preprocess_text)).all())
 
+'''  vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+X_ngrams = vectorizer.fit_transform(processed)
+X_ngrams.shape
+
+#####Treinamento e Avaliação
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_ngrams,
+    y_enc,
+    test_size=0.2,
+    random_state=42,
+    stratify=y_enc
+)
+
+clf = svm.LinearSVC(loss='hinge')
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+print(metrics.f1_score(y_test, y_pred))
+
+pd.DataFrame(
+    metrics.confusion_matrix(y_test, y_pred),
+    index=[['actual', 'actual'], ['spam', 'ham']],
+    columns=[['predicted', 'predicted'], ['spam', 'ham']]
+)
+
+sample_space = np.linspace(500, len(raw_text) * 0.8, 10, dtype='int')
+
+train_sizes, train_scores, valid_scores = learning_curve(
+    estimator=svm.LinearSVC(loss='hinge', C=1e10),
+    X=X_ngrams,
+    y=y_enc,
+    train_sizes=sample_space,
+    cv=StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=40),
+    scoring='f1',
+    n_jobs=-1
+)
+
+def make_tidy(sample_space, train_scores, valid_scores):
+    messy_format = pd.DataFrame(
+        np.stack((sample_space, train_scores.mean(axis=1),
+                  valid_scores.mean(axis=1)), axis=1),
+        columns=['# of training examples', 'Training set', 'Validation set']
+    )
+    
+    return pd.melt(
+        messy_format,
+        id_vars='# of training examples',
+        value_vars=['Training set', 'Validation set'],
+        var_name='Scores',
+        value_name='F1 score'
+    )
+
+
+grid_search.fit(X_ngrams, y_enc)
+final_clf = svm.LinearSVC(loss='hinge', C=grid_search.best_params_['C'])
+final_clf.fit(X_ngrams, y_enc);
+
+pd.Series(
+    final_clf.coef_.T.ravel(),
+    index=vectorizer.get_feature_names()
+).sort_values(ascending=False)[:20]
+
+def spam_filter(message):
+    if final_clf.predict(vectorizer.transform([preprocess_text(message)])):
+        return 'spam'
+    else:
+        return 'not spam'
+
+print(spam_filter(example))
+
+print(spam_filter('Ohhh, but those are the best kind of foods'))
+
+'''
