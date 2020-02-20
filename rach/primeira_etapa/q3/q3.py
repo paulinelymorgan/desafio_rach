69da1c414ce6d009c58b4a494a72cdbd80a0d3e3
Paulinely Morgan <paulinelym@gmail.com>
2020-02-19 23:00:31 -0300

    melhorias q3

--- rach/primeira_etapa/q3/q3.py
+++ rach/primeira_etapa/q3/q3.py
@@ -15,17 +15,18 @@ questao3 = pd.DataFrame({'Date': pd.to_datetime(base.Date), 'Word_Count': base.W
 questao3['Month/Year'] = questao3['Date'].apply(lambda x: "%d/%d" % (x.month, x.year))
 
 # agrupa pelos dois parametros (o novo gerado e 'Word_Count')
-print(questao3)
+# print(questao3)
 agrupado_mes_q3 = questao3.groupby(['Month/Year'])
 
-print()
+# algumas metricas do agrupamento
+print(agrupado_mes_q3.describe())
 
-print("\nmax: {}".format(agrupado_mes_q3.max().rename(columns={'Word_Count':'MAX'})))
-print("\nmin: {}".format(agrupado_mes_q3.min().rename(columns={'Word_Count':'MIN'})))
-print("\nmean: {}".format(agrupado_mes_q3.mean().rename(columns={'Word_Count':'MEAN'})))
-print("\nmedian: {}".format(agrupado_mes_q3.median().rename(columns={'Word_Count':'MEDIAN'})))
-print("\nstd: {}".format(agrupado_mes_q3.std().rename(columns={'Word_Count':'STD'})))
+# print("\nmax: {}".format(agrupado_mes_q3.max().rename(columns={'Word_Count':'MAX'})))
+# print("\nmin: {}".format(agrupado_mes_q3.min().rename(columns={'Word_Count':'MIN'})))
+# print("\nmean: {}".format(agrupado_mes_q3.mean().rename(columns={'Word_Count':'MEAN'})))
+# print("\nmedian: {}".format(agrupado_mes_q3.median().rename(columns={'Word_Count':'MEDIAN'})))
+# print("\nstd: {}".format(agrupado_mes_q3.std().rename(columns={'Word_Count':'STD'})))
 print("\nvar: {}".format(agrupado_mes_q3.var().rename(columns={'Word_Count':'VARIANCE'})))
-print("\nsize: {}".format(agrupado_mes_q3.size()))
+# print("\nsize: {}".format(agrupado_mes_q3.size()))
 
 #### QUESTAO 3 - varias metricas
